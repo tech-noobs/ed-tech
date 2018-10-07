695dcb777f4e06b9f2f6e5e6e845ac586285bc98
sahil <sahiljdoshi@gmail.com>
2018-10-07 05:49:09 +0530

    Login Added

---
+++ app/src/main/java/com/technoobs/edtech/LiveClassActivity.java
@@ -0,0 +1,13 @@
+package com.technoobs.edtech;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LiveClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_class);
+    }
+}
